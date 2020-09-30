aea74558d31b0f8b7ce3250be1b1615530abb0d1
Khandaker Mobashyer Hossain <jokerjamil007@gmail.com>
2020-09-30 16:28:11 +0600

    first commit

---
+++ newLN.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup as soup
+from docx import Document
+import regex as re
+
+def main():
+    print("Choose your preferred site?")
+    print("1. wuxiaworld.co")
+    print("2. ")
+
+if __name__=="__main__":
+    main()
