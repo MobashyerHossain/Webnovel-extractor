ac6ee02cf3edf3f85e4ae22f467dfe149fb2728c
Khandaker Mobashyer Hossain <jokerjamil007@gmail.com>
2020-09-30 16:51:51 +0600

    website options

--- newLN.py
+++ newLN.py
@@ -2,10 +2,27 @@ from bs4 import BeautifulSoup as soup
 from docx import Document
 import regex as re
 
+wuxia_world():
+    print('Welcome to Wuxia World!')
+    print('1. List of Light Novel')
+    print('2. List of Genre')
+    print('3. Search by name')
+    novel = input('Name of Novel: ')
+
+
+novel_full():
+    return 1
+
 def main():
     print("Choose your preferred site?")
-    print("1. wuxiaworld.co")
-    print("2. ")
+    print("1. Wuxiaworld")
+    print("2. Novelfull")
+    site_choice = int(input("Choice: "))
+
+    if site_choice == 1:
+        wuxia_world()
+    else:
+        novel_full()
 
 if __name__=="__main__":
     main()
